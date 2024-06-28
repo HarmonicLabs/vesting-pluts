7013db243cf62146dc6e9ffdb6e0fbab0946235a
RSoulatIOHK <romain.soulat@iohk.io>
2024-06-28 14:41:30 +0200

    Update genKeys.ts

--- src/app/genKeys.ts
+++ src/app/genKeys.ts
@@ -12,7 +12,7 @@ config();
 
 async function genKeys()
 {
-    const nKeys = 2;
+    const nKeys = 1;
 
     const promises: Promise<any>[] = [];
 
@@ -34,41 +34,51 @@ async function genKeys()
               true,
               ["sign", "verify"]
           );
-          
+        // From export in raw
         const publicKeyArrayBuffer = await globalThis.crypto.subtle.exportKey('raw', keyPair.publicKey);
         const publicKeyUint8Array = new Uint8Array(publicKeyArrayBuffer);
         const publicKey = new PublicKey(publicKeyUint8Array);
         const publicKeyHash = new PubKeyHash(blake2b(publicKeyUint8Array, undefined, 28));
-
         const pubKeyJsonObj = {
             type: "PaymentVerificationKeyShelley_ed25519",
             description: "Payment Verification Key",
             cborHex: publicKey.toCbor().toString()
         }
+        console.log("\nPublic Key:",publicKey.toCbor().toString());
 
         const pubKeyJsonStr = JSON.stringify(pubKeyJsonObj, null, 4);
         await writeFile(`./testnet/payment_tests${i}.vkey`, pubKeyJsonStr);
 
-        const privateKeyArrayBuffer = await globalThis.crypto.subtle.exportKey('raw', keyPair.privateKey);
-        const privateKeyUint8Array = new Uint8Array(privateKeyArrayBuffer);
+        // Export of the private key in a way that's compatible with the Cardano CLI
+        const privateKeyArrayBuffer = await globalThis.crypto.subtle.exportKey('pkcs8', keyPair.privateKey);
+        const privateKeyUint8Array = new Uint8Array(privateKeyArrayBuffer.slice(-32));
         const privateKey = new PrivateKey(privateKeyUint8Array);
 
+        // Check that the derivations went fine
+        const pubKeyfromPriv = privateKey.derivePublicKey();
+        console.log("\nPrivate Key:",privateKey.toCbor().toString());
+        console.log("\nChecking the correspondance between private and public key:")
+        if (pubKeyfromPriv.toString() !== publicKey.toString()) {
+            throw new Error("\tPublic key derivation from private key failed");
+        }
+        else {
+            console.log("\tPublic key derivation from private key succeeded");
+        }
+  
         const pvtKeyJsonObj = {
             type: "PaymentSigningKeyShelley_ed25519",
             description: "Payment Signing Key",
             cborHex: privateKey.toCbor().toString()
         }
 
-        const pvtKeyJsonStr = JSON.stringify(pvtKeyJsonObj, null, 4);
-        await writeFile(`./testnet/payment_tests${i}.skey`, pvtKeyJsonStr);
+        const pvtKeyJsonStr = JSON.stringify(pubKeyJsonObj, null, 4);
+        await writeFile(`./testnet/payment_tests${i}.vkey`, pvtKeyJsonStr);
 
-        console.log(publicKey.toCbor().toString()); 
-        
+
         // Create the address
         const credential = Credential.keyHash(publicKeyHash);
         const address = new Address("testnet", credential);
-        console.log(address.toString());
-        
+        console.log("\nAddress:",address.toString());
     }
 
     // wait for all files to be copied
