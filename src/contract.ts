e95dee0a8cb3875ddd91627d5bd9d83862b11221
amnambiar <anusree.nambiar@iohk.io>
2025-11-10 19:48:09 +0530

    Fixed compilation

--- src/contract.ts
+++ src/contract.ts
@@ -3,7 +3,7 @@ import { Compiler, createMemoryCompilerIoApi } from '@harmoniclabs/pebble';
 import { Script, ScriptType, Address, Credential } from "@harmoniclabs/buildooor";
 import { fromUtf8 } from "@harmoniclabs/uint8array-utils";
 
-const CONTRACT_NAME = 'index.pebble';
+const CONTRACT_NAME = 'src/vesting.pebble'
 
 const CONTRACT = fs.readFileSync(CONTRACT_NAME, 'utf8');
 
@@ -17,9 +17,7 @@ async function compileContract(): Promise<Uint8Array> {
 
   const compiler = new Compiler(ioApi);
 
-  await compiler.compile({ entry: CONTRACT_NAME, root: "/", outDir: 'dist' });
-
-  const compiled = ioApi.outputs.get("dist/out.flat");
+  const compiled = await compiler.compile({ entry: CONTRACT_NAME });
 
   return compiled || new Uint8Array();
 }
